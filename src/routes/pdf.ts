a470d798e9b15f4c8764151dc3ee60a7ca9a7eca
Vladimir Bondarenko <vbondarenko@teh-lab.ru>
2022-10-04 01:35:28 +0300

    feat(routes) add process method to pdf

--- src/routes/pdf.ts
+++ src/routes/pdf.ts
@@ -1,6 +1,46 @@
 import { Request, Response } from 'express';
 import { Route } from '../classes';
+import { Pattern } from '../interfaces';
 import { PdfService } from '../services';
+
+const EXAMPLE_PATTERNS: Pattern[] = [
+  {
+    variable: 'first',
+    value: 'Один',
+  },
+  {
+    variable: 'second',
+    value: 'Два',
+  },
+  {
+    variable: 'third',
+    value: 'Три',
+  },
+  {
+    variable: 'table1',
+    value: 'Таблица 1',
+  },
+  {
+    variable: 'table2',
+    value: 'Таблица 2',
+  },
+  {
+    variable: 'table3',
+    value: 'Таблица 3',
+  },
+  {
+    variable:'table4' ,
+    value: 'Таблица 4',
+  },
+  {
+    variable:'table5' ,
+    value: 'Таблица 5',
+  },
+  {
+    variable:'table6' ,
+    value: 'Таблица 6',
+  },
+]
 
 export class PdfRoute extends Route {
   private readonly pdfService: PdfService;
@@ -14,7 +54,13 @@ export class PdfRoute extends Route {
     this.pdfService = new PdfService();
   }
 
-  public process(): void {
+  public async process(): Promise<void> {
+    const patterns: Pattern[] = this.request.body?.patterns ?? EXAMPLE_PATTERNS;
 
+    this.pdfService.replaceVariablesWithValues(patterns);
+
+    const buffer = await this.pdfService.getOutputStreamBuffer();
+
+    this.response.contentType('application/pdf').send(buffer);
   }
 }
