2a8522c89e166a6879db790e9225b8abb4f21163
Vladimir Bondarenko <vbondarenko@teh-lab.ru>
2022-10-05 21:08:50 +0300

    feat(interfaces) add loop pattern

--- src/interfaces/pattern.ts
+++ src/interfaces/pattern.ts
@@ -1,4 +1,7 @@
+import { LoopPattern } from "./loop-pattern";
+import { TablePattern } from "./table-pattern";
+
 export interface Pattern {
   variable: string,
-  value: string,
+  value: string | LoopPattern | TablePattern,
 }
