e7e983d20640af72933df07424ed764636d5e1d5
Vladimir Bondarenko <vbondarenko@teh-lab.ru>
2022-10-07 06:20:19 +0300

    fix(interfaces) remove deleted type from interface's property

--- src/interfaces/pattern.ts
+++ src/interfaces/pattern.ts
@@ -1,7 +1,6 @@
 import { LoopPattern } from "./loop-pattern";
-import { TablePattern } from "./table-pattern";
 
 export interface Pattern {
   variable: string,
-  value: string | LoopPattern | TablePattern,
+  value: string | LoopPattern,
 }
